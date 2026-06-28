80b57f547ee8e6ceb4a30e7f5ee6de712df7f2bd
nuclearoreo <nuclearoreo@gmail.com>
2026-06-28 12:38:14 -0400

    Went crazy

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use actor::ActorHandle;
 #[tokio::main]
 async fn main() {
     let handle = ActorHandle::new();
-    let mut val = handle.get_unique_id().await;
+    let val = handle.get_unique_id().await;
     println!("{:?}", val);
 }
